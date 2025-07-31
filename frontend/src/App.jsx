d5fe7ad47d33c01639a1d3be6092250696902b71
Andrzej-Tezyk <andrewtezhik@gmail.com>
2025-07-31 18:39:46 +0200

    folder structure for front

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+const today = new Date();
+// function App() {} an old way to define components
+
+//toLocaleDateString
+const  App = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/TrackedActivities.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TrackedActivities = () => {
+    return ( 
+        <div>
+            <h2>Activities</h2>
+            <div>
+                
+            </div>
+        </div>
+     );
+}
+ 
+export default ;

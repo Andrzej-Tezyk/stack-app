7e18a2eee49e5e5c0dd8fe55bd78bb32e231b351
Andrzej-Tezyk <andrewtezhik@gmail.com>
2025-08-01 15:03:07 +0200

    dashboard layout

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,9 +1,8 @@
 function Dashboard () {
     return ( 
-        <div className="border-2 m-3 w-md h-1/2 border-solid">
-            <h2>Dashboad</h2>
-            <div>
-
+        <div className="flex border-2 m-3 w-md h-1/2 border-solid justify-center items-center">
+            <div className="border-1 flex-grow m-5">
+                Dashboard (cirkle)
             </div>
         </div>        
      );
